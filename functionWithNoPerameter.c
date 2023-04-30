21814569a2a4d78089d0f37d6705fcfc3b89383d
srayo16 <srayosikder@gmail.com>
2023-05-01 01:02:33 +0600

    second

--- functionWithNoPerameter.c
+++ functionWithNoPerameter.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+int sumTwo()
+{
+    int a, b;
+    printf("Enter two digits: ");
+    scanf("%d %d", &a, &b);
+    int res = a + b;
+    return res;
+}
+
 int main()
 {
+    printf("%d", sumTwo());
 
     return 0;
 }
